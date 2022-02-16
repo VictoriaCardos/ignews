d670391b31febe5d0961bc4b1c8100bad98397e9
VictoriaCardos <vvjcerque@hotmail.com>
2022-02-16 17:38:00 -0300

    feat: create autentication for github

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -5,7 +5,12 @@ export default NextAuth({
   providers:[
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      authorization: {
+        params: {
+          scope: 'read:user',
+        },
+      },
     }), 
   ],
 
